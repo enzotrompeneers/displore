433fdd25558a1f513e5f5e8cac1cf3498f876e0e
enzotrompeneers <trompeneers@telenet.be>
2017-10-04 15:32:08 +0200

    adding routes

--- displore/routes/web.php
+++ displore/routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'HomeController@index'); 
+Route::get('login', 'LoginController@index');
+Route::get('register', 'RegisterController@index');
+Route::get('detail', 'DetailController@index');
+Route::get('offer', 'OfferController@index');
+Route::get('discover', 'DiscoverController@index');
+Route::get('discover/{id}', 'DiscoverController@show');
