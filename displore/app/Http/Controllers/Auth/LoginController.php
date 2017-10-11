054b8b53025e73b5737fc5fc0e93df30b7a04d3d
Vincent De Coen <vincent.decoen94@gmail.com>
2017-10-11 13:00:21 +0200

    verkeerde redirect fix

--- displore/app/Http/Controllers/Auth/LoginController.php
+++ displore/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'discover';
+    protected $redirectTo = 'ontdek';
 
     /**
      * Create a new controller instance.
