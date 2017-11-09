47befaae4dc17b7fe4dfb513c9da061948b9ba6d
Vincent De Coen <vincent.decoen94@gmail.com>
2017-11-09 01:31:13 +0100

    modal optimalizatie voor mobiel en algemeen

--- displore/resources/views/components/modal.blade.php
+++ displore/resources/views/components/modal.blade.php
@@ -1,10 +1,10 @@
 <div class="modal-overlay {{ isset($overlay_class) ? trim($overlay_class) : ""}}" id="modal-overlay">
 	<div class="modal {{ isset($class) ? trim($class) : ""}}" id="{{ isset($id) ? trim($id) : ""}}">
 		<div class="modal-title row">
-			<div class="medium-10 columns">
+			<div class="small-10 medium-10 columns">
 				<h1 class="modal-title-text">{{ $title or ""}}</h1>
 			</div>
-			<div class="medium-2 columns">
+			<div class="small-2 medium-2 columns">
 				<a href="{{ isset($cancel_link) ? trim($cancel_link) : "" }}" id="modal-close">
 					<img src="{{ asset('assets/graphics/close_icon_black.svg') }}" class="modal-close right" height="40px" width="40px">
 				</a>
