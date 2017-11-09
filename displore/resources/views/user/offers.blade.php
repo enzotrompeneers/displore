182f28c94e26682a022922617e2de41851898d6b
Vincent De Coen <vincent.decoen94@gmail.com>
2017-11-09 01:07:14 +0100

    consistente padding bij profiel tabs

--- displore/resources/views/user/offers.blade.php
+++ displore/resources/views/user/offers.blade.php
@@ -6,23 +6,27 @@
 		@slot('tab')
 			offers
 		@endslot
-		<h1>Jouw aanbiedingen</h1>
-		@foreach($products as $product)
-			<div class="reservation">
-				<div class="reservation-text">
-					{{ $product->title }}
-				</div>
-				<div class="reservation-info">
-					<a class="button" href="{{ route('availability.create', $product->id) }}">Geef je beschikbaarheid aan</a>
-					<a class="red_ghost" href="{{ route('product.edit', $product->id) }}">Bewerken</a>
-				</div>
-				
-			</div>
-		@endforeach
+		<div class="row">
+			<div class="small-12 medium-12 large-12 columns">
+				<h1>Jouw aanbiedingen</h1>
+				@foreach($products as $product)
+					<div class="reservation">
+						<div class="reservation-text">
+							{{ $product->title }}
+						</div>
+						<div class="reservation-info">
+							<a class="button" href="{{ route('availability.create', $product->id) }}">Geef je beschikbaarheid aan</a>
+							<a class="red_ghost" href="{{ route('product.edit', $product->id) }}">Bewerken</a>
+						</div>
+						
+					</div>
+				@endforeach
 
-		@if(sizeof($products) === 0)
-			<h3 class="search-fail">Je bied nog niets aan!</h3>
-		@endif
+				@if(sizeof($products) === 0)
+					<h3 class="search-fail">Je bied nog niets aan!</h3>
+				@endif
+			</div>
+		</div>
 	@endcomponent
 	
 @endsection
--- displore/resources/views/user/reservations.blade.php
+++ displore/resources/views/user/reservations.blade.php
@@ -6,63 +6,68 @@
 		@slot('tab')
 			reservations
 		@endslot
-		<h1>Reservaties bij jou</h1>
-
-		@foreach($products as $product)
-			@foreach($product->reservations()->get() as $reservation)
+		<div class="row">
+			<div class="small-12 medium-12 large-12 columns">
 				
-				<div class="reservation">
-					<div class="reservation-text">{{ $reservation->product->title }}</div>
+				
+				<h1>Reservaties bij jou</h1>
+
+				@foreach($products as $product)
+					@foreach($product->reservations()->get() as $reservation)
+						
+						<div class="reservation">
+							<div class="reservation-text">{{ $reservation->product->title }}</div>
 
 
-					<div class="reservation-info">
-						Door <a href="mailto: {{ $reservation->user->email }}">{{ $reservation->user->first_name . " " . $reservation->user->last_name }}</a>
+							<div class="reservation-info">
+								Door <a href="mailto: {{ $reservation->user->email }}">{{ $reservation->user->first_name . " " . $reservation->user->last_name }}</a>
 
-						Op {{ Carbon\Carbon::parse($reservation->from)->toDateString() }}
-						@if($reservation->product->price_time === "day")
-							Tot en met
-							{{ Carbon\Carbon::parse($reservation->to)->toDateString() }}
-						@endif
-					
-					</div>
-				</div>
-			@endforeach
-		@endforeach
+								Op {{ Carbon\Carbon::parse($reservation->from)->toDateString() }}
+								@if($reservation->product->price_time === "day")
+									Tot en met
+									{{ Carbon\Carbon::parse($reservation->to)->toDateString() }}
+								@endif
+							
+							</div>
+						</div>
+					@endforeach
+				@endforeach
 
-		@if(sizeof($products) === 0)
-			<h3 class="search-fail">Nog niemand heeft bij je gereserveerd!</h3>
-		@endif
-
-		<h1>Reservaties bij andere</h1>
-	
-		@foreach($my_reservations as $reservation)
-			<div class="reservation">
-				<div class="reservation-text">{{ $reservation->product->title }}</div>
-	
-				<div class="reservation-info">
-					Door <a href="mailto: {{ $reservation->user->email }}">{{ $reservation->user->first_name . " " . $reservation->user->last_name }}</a>
-
-					Op {{ Carbon\Carbon::parse($reservation->from)->toDateString() }}
-					@if($reservation->product->price_time === "day")
-						Tot en met
-						{{ Carbon\Carbon::parse($reservation->to)->toDateString() }}
-					@endif
-				</div>
-
-				@if(!$reservation->paid)
-				<div class="right">
-					<a href="{{ route('reservation.payment', $reservation->id) }}" class="button">Betalen</a>
-				</div>
+				@if(sizeof($products) === 0)
+					<h3 class="search-fail">Nog niemand heeft bij je gereserveerd!</h3>
 				@endif
-			</div>
+
+				<h1>Reservaties bij andere</h1>
 			
-		@endforeach
+				@foreach($my_reservations as $reservation)
+					<div class="reservation">
+						<div class="reservation-text">{{ $reservation->product->title }}</div>
+			
+						<div class="reservation-info">
+							Door <a href="mailto: {{ $reservation->user->email }}">{{ $reservation->user->first_name . " " . $reservation->user->last_name }}</a>
 
-		@if(sizeof($my_reservations) === 0)
-			<h3 class="search-fail">Je hebt nog geen reservaties bij anderen gedaan!</h3>
-		@endif
+							Op {{ Carbon\Carbon::parse($reservation->from)->toDateString() }}
+							@if($reservation->product->price_time === "day")
+								Tot en met
+								{{ Carbon\Carbon::parse($reservation->to)->toDateString() }}
+							@endif
+						</div>
 
+						@if(!$reservation->paid)
+						<div class="right">
+							<a href="{{ route('reservation.payment', $reservation->id) }}" class="button">Betalen</a>
+						</div>
+						@endif
+					</div>
+					
+				@endforeach
 
+				@if(sizeof($my_reservations) === 0)
+					<h3 class="search-fail">Je hebt nog geen reservaties bij anderen gedaan!</h3>
+				@endif
+
+			</div>
+		</div>
 	@endcomponent
 
 @endsection
