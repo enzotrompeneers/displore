1f7e5ab914e69277686948574f04885e54357190
enzotrompeneers <trompeneers@telenet.be>
2017-11-10 00:10:15 +0100

    reset password view

--- displore/resources/views/auth/passwords/reset.blade.php
+++ displore/resources/views/auth/passwords/reset.blade.php
@@ -15,13 +15,38 @@
             <form class="form-horizontal" method="POST" action="{{ route('password.request') }}" data-abide novalidate>
                 {{ csrf_field() }}
 
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">Nieuw Wachtwoord</label>
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                <input type="hidden" name="token" value="{{ $token }}">
 
-                       @if ($errors->has('email'))
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">E-Mail Adres</label>
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required pattern="email">
+                        <span class="error">Verplicht veld, geef een geldig E-mail adres!</span>
+                        @if ($errors->has('email'))
                             <span>{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="col-md-4 control-label">Nieuw Wachtwoord</label>
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control" name="password" required pattern="^(?=.*[0-9])(?=.*[a-z]).{6,}$">
+                        <span class="error">Verplicht veld, min. 6 karakters, minstens 1 letter en 1 nummer</span>
+                        @if ($errors->has('password'))
+                            <span>{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                    <label for="password" class="col-md-4 control-label">Bevestig Wachtwoord</label>
+                    <div class="col-md-6">
+                        <input id="password-confirm" type="password" class="form-control" name="password-confirm" required pattern="^(?=.*[0-9])(?=.*[a-z]).{6,}$">
+                        <span class="error">Verplicht veld, min. 6 karakters, minstens 1 letter en 1 nummer</span>
+                        <span class="error">Wachtwoord is niet hetzelfde!</span>
+                        @if ($errors->has('password_confirmation'))
+                            <span>{{ $errors->first('password_confirmation') }}</span>
                         @endif
                     </div>
                 </div>
